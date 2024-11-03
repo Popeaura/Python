3b02678d496f00078fe562154ac05d2ee9512f6d
Popeaura <popeahura@gmail.com>
2024-11-04 00:12:09 +0300

    I added and modified my user_input.py file

---
+++ user_input.py
@@ -0,0 +1,13 @@
+#user input
+ 
+# Ask for user name
+name = input("What is your name ?")
+
+#Ask for user age
+age =  input("What is your age")
+
+#Ask user's location 
+location = input("Where do you live?")
+
+#print personal info
+print(g"Hello{name}, you are{age}, years old and i suppose you stay in{location}")
