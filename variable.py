ad8dd8d7f4ae4b9210374cf234c6c5d549acfd46
Popeaura <popeahura@gmail.com>
2024-11-07 16:39:25 +0300

    I modified my folder

--- variable.py
+++ variable.py
@@ -1,2 +1,16 @@
 site_name = "Power Learn Project"
 print(site_name)
+
+# Assigning new value to site_name
+site_name = "I love coding 😊"
+print(site_name)
+
+a,b,c = 5, 7, "Hello world"
+print(a) # prints 5
+print(b) # prints 5
+print(c) # prints Hello World
+
+num = 55
+Num = 510
+print(num) #5
+print(Num) #510
