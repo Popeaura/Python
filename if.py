80b3a67ed701c37975fbea42a4b709915c6f0677
Popeaura <popeahura@gmail.com>
2024-11-13 15:33:48 +0300

    I added and modified my project's folder

--- if.py
+++
@@ -1,9 +0,0 @@
-test_value = 100
-
-if test_value > 1:
-    print("This code is executed !")
-
-if test_value > 1000:
-    print("This code is Not executed !")
-
-print("Program continues at this point ")
