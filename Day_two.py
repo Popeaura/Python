f1472d3b90e079644330dd96edc3d72623a2c7d0
Popeaura <popeahura@gmail.com>
2024-11-13 15:43:55 +0300

    I added and modified my project's folder

--- Day_two.py
+++ Day_two.py
@@ -8,10 +8,20 @@ if test_value > 1000:
 
 print("Program continues at this point ")
 
+###test_value = 61
 
-test_value = 61
+###if test_value > 60:
+   ### print("You Passed ")
+###else :
+   ### print("You failed")
 
-if test_value > 60:
-    print("You Passed ")
-else :
-    print("You failed")
+pet_type = "geko"
+
+if pet_type == "dog":
+    print("You have a puppy ")
+elif pet_type == "cat":
+     print("You have a cat")
+elif pet_type == "fish":
+    print("You have a fish")
+else: 
+    print("Not sure")
