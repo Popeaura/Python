4b7f14f3290e24a738b8c92d47b074055423f219
Popeaura <popeahura@gmail.com>
2024-11-13 15:33:36 +0300

    I added and modified my project's folder

---
+++ Day_two.py
@@ -0,0 +1,17 @@
+test_value = 100
+
+if test_value > 1:
+    print("This code is executed !")
+
+if test_value > 1000:
+    print("This code is Not executed !")
+
+print("Program continues at this point ")
+
+
+test_value = 61
+
+if test_value > 60:
+    print("You Passed ")
+else :
+    print("You failed")
