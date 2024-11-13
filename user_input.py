1cb685186af81e128681c762e669cb3ee833e305
Popeaura <popeahura@gmail.com>
2024-11-13 15:15:53 +0300

    I modfified my project's folder

--- user_input.py
+++ user_input.py
@@ -11,3 +11,13 @@ location = input("Where do you live?")
 
 #print personal info
 print(f"Hello {name}, you are {age}, years old and i suppose you stay in {location}")
+
+
+number = int(input('Enter your Admission number here :'))
+
+#check if the number is valid
+if number > 0 :
+    print(f'{number} is now fully registered to the system.')
+    else {
+print('Number input is invalid cannot be registered to our systems.')
+    }
