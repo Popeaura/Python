88188065d091da11c976fdfa1fd967294fef54d4
Popeaura <popeahura@gmail.com>
2024-11-20 23:44:19 +0300

    I modified my projects folder

--- operations.py
+++ operations.py
@@ -4,3 +4,12 @@ with open("sample.txt", "r") as file:
 
 with open("sweet.txt", "w") as file:
     file.write("Hello, Python!")
+
+# Read from the file
+with open("sweet.txt", "r") as file:
+    content = file.read()
+    print(content)    
+
+with open("sample.txt", "r") as file:
+    data = file.read()
+    print(data)
