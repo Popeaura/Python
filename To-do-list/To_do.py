ccaac29c4e3ae8794f7f1df886a1f8ff0e204de4
Popeaura <popeahura@gmail.com>
2024-11-29 14:18:08 +0300

    Modified my projects  files

--- To-do-list/To_do.py
+++ To-do-list/To_do.py
@@ -8,9 +8,18 @@ def display_task():
         else:
         print("\Your to -do list :")
         for index, task in enumerate(task, 1):
-            print(f "{index}.{task}")
+            print(f"{index}.{task}")
 
 def display_task():
         task = input("Enter the task:")
         tasks.append(task)
         print(f"Task '{task}' added successfully !")
+
+ def remove_task():
+    display_task()
+    try:
+        task_number = int(input("Enter the task number to remove: "))
+        removed_task = tasks.pop(task_number - 1)
+        print(f"Task '{removed_task}' removed successfully!")
+    except (ValueError, IndexError):
+        print("Invalid task number!")
