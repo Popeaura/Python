2c46653ea6aec04be9f0de8f0d0dc75988b90f90
Popeaura <popeahura@gmail.com>
2024-11-29 13:55:30 +0300

    Modified my projects  files

--- To-do-list/To_do.py
+++ To-do-list/To_do.py
@@ -1,8 +1,16 @@
 # To do list 
 
+tasks = []
+
 def display_task():
     if not task:
         print("\nYour to-do list is empty!")
         else:
         print("\Your to -do list :")
-        
+        for index, task in enumerate(task, 1):
+            print(f "{index}.{task}")
+
+def display_task():
+        task = input("Enter the task:")
+        tasks.append(task)
+        print(f"Task '{task}' added successfully !")
