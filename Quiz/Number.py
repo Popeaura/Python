552f9a862a98832560b26bedd9f4717fe5cdbb29
Popeaura <popeahura@gmail.com>
2024-12-03 20:17:46 +0300

    I added my modified projec's folder

--- Quiz/Number.py
+++ Quiz/Number.py
@@ -1,16 +1,18 @@
 import random
-
-top_of_range = input("Type a number: ")
+top_of_range = input ("Type a number :")
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
-    if top_of_range <= 0:
-        print("Please type a number larger than 0 next time.")
-        quit()
-else:
-    print("Please type a number next time.")
+if  top_of_range <= 0:
+    print("Please type a number larger than 0 next time.")
+    quit()
+
+else: 
+    print("Please type a number next time") 
     quit()
 
 random_number = random.randint(0, top_of_range)
-print(f"The random number between 0 and {top_of_range} is: {random_number}")
+print(random_number)
+
+
