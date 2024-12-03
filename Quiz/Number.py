1fd1d8e5a8b0f6c48b5fde83f3103e26862b5fca
Popeaura <popeahura@gmail.com>
2024-12-03 20:45:03 +0300

    I added my modified projec's folder

--- Quiz/Number.py
+++ Quiz/Number.py
@@ -8,19 +8,29 @@ if top_of_range.isdigit():
         print("Please type a number larger than 0 next time.")
         quit()
 else:
-    print("Please type a number next time.")
+    print("Please type a valid number next time.")
     quit()
 
 random_number = random.randint(0, top_of_range)
+print(f"A random number between 0 and {top_of_range} has been generated.")
 
 while True:
-    user_guess = input("Make a guess :")
+    user_guess = input("Make a guess: ")
+
     if user_guess.isdigit():
         user_guess = int(user_guess)
 
-    if user_guess <= 0:
-        print("Please type a number larger than 0 next time.")
-        quit()
+        if user_guess < 0:
+            print("Please type a number larger than 0.")
+            continue
+
+        # Compare user_guess with random_number
+        if user_guess == random_number:
+            print("Congratulations! You guessed the correct number.")
+            break
+        elif user_guess < random_number:
+            print("Too low. Try again!")
+        else:
+            print("Too high. Try again!")
     else:
-        print("Please type a number next time.")
-        quit()
+        print("Please type a valid number.")
