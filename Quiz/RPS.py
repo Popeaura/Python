6b205ceafd5662459b8d7e2b41471b800b01d66d
Popeaura <popeahura@gmail.com>
2024-12-04 22:25:08 +0300

    I added my modified projec's folder

--- Quiz/RPS.py
+++ Quiz/RPS.py
@@ -3,5 +3,18 @@ import random
 user_wins = 0
 computer_wins = 0
 
+options =  ["Rock", "Paper", "Scissors"]
+
 while True:
-    user_input = input("Type Rock/Paper/scissors or Q to quit")
+    user_input = input("Type Rock/Paper/scissors or Q to quit : ").lower()
+    if user_input ==  "q": 
+        break
+
+    if user_input not in options:
+        continue
+
+    random_number = random.int(0, 2) 
+    #   rock: 0, paper:1 , scissors: 2
+
+
+print("Goodbye !")    
