1a42a644dab5b2b5d1822a52d568131e7fd8a0d9
Popeaura <popeahura@gmail.com>
2024-12-04 22:46:00 +0300

    I added my modified projec's folder

--- Quiz/RPS.py
+++ Quiz/RPS.py
@@ -3,18 +3,34 @@ import random
 user_wins = 0
 computer_wins = 0
 
-options =  ["Rock", "Paper", "Scissors"]
+options = ["rock", "paper", "scissors"]
 
 while True:
-    user_input = input("Type Rock/Paper/scissors or Q to quit : ").lower()
-    if user_input ==  "q": 
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
         break
 
     if user_input not in options:
+        print("Invalid input. Please type Rock, Paper, or Scissors.")
         continue
 
-    random_number = random.int(0, 2) 
-    #   rock: 0, paper:1 , scissors: 2
+    random_number = random.randint(0, 2)  # Generate a random number between 0 and 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
 
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You Won!")
+        user_wins += 1
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You Won!")
+        user_wins += 1
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You Won!")
+        user_wins += 1
+    else:
+        print("You Lost!")
+        computer_wins += 1
 
-print("Goodbye !")    
+print("You won", user_wins, "times.")
+print("Computer won", computer_wins, "times.")
+print("Goodbye!")
