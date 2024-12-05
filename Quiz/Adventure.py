a62ec558e802b6dd53f5a22674273aac9c5ae16d
Popeaura <popeahura@gmail.com>
2024-12-05 23:46:21 +0300

    I added my modified projec's folder

--- Quiz/Adventure.py
+++ Quiz/Adventure.py
@@ -1,2 +1,4 @@
 name = input("Enter your name to continue with the game")
 print("Welcome", name, "to this Adventure !")
+
+answer =  input("You are on a dirt road it has come to an end and you can go left or right  ,which way would like to go ")
