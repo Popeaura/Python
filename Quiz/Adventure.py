5acdf863b147ab850152ad7d0a7d7ca4411985d3
Popeaura <popeahura@gmail.com>
2024-12-05 23:48:24 +0300

    I added my modified projec's folder

--- Quiz/Adventure.py
+++ Quiz/Adventure.py
@@ -4,4 +4,4 @@ print("Welcome", name, "to this Adventure !")
 answer =  input("You are on a dirt road it has come to an end and you can go left or right  ,which way would like to go ?").lower()
 
 
-if answer = left
+if answer == "left":
