b84cbd69b705c251c95dcaba5cbb4d6948db2fff
Popeaura <popeahura@gmail.com>
2024-12-07 23:19:53 +0300

    I added my modified projec's folder

--- Quiz/Adventure.py
+++ Quiz/Adventure.py
@@ -1,38 +1,36 @@
-name = input("Enter your name to continue with the game")
-print("Welcome", name, "to this Adventure !")
+name = input("Enter your name to continue with the game: ")
+print("Welcome", name, "to this Adventure!")
 
-answer =  input("You are on a dirt road it has come to an end and you can go left or right  ,which way would like to go ?").lower()
-
+answer = input("You are on a dirt road, it has come to an end, and you can go left or right. Which way would you like to go? (left/right): ").lower()
 
 if answer == "left":
-    answer = input("You come to a river you can walk around it or swim  to swim across ? Type walk around and to swim across: ")
+    answer = input("You come to a river. You can walk around it or swim across. Type 'walk' to walk around or 'swim' to swim across: ").lower()
 
-if answer ==  "swim":
-    print('You swam across and were eaten by alligator')
+    if answer == "swim":
+        print("You swam across and were eaten by an alligator.")
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water, and lost the game.")
+    else:
+        print("Not a valid option. You lose.")
 
-elif answer == "walk":
-    print('You walked for many miles , ran out of water and yiu lost the game')
+elif answer == "right":
+    answer = input("You came to a bridge that looks wobbly. Do you want to go across it or head back? (cross/back): ").lower()
+
+    if answer == "back":
+        print("You go back and lose.")
+    elif answer == "cross":
+        answer = input("You crossed the bridge and met a stranger. Do you talk to them? (yes/no): ").lower()
+
+        if answer == "yes":
+            print("You talk to the stranger and they give you gold. You WIN!!")
+        elif answer == "no":
+            print("You ignore the stranger. They are offended, and you lose.")
+        else:
+            print("Not a valid option. You lose.")
+    else:
+        print("Not a valid option. You lose.")
 
 else:
-    print('Not a valid option you lose')
+    print("Not a valid option. You lose.")
 
-elif answer == "right"
-answer = input('You came to a bridge which looks wobbly do you want to go across it or head back(cross/back) ?')
-
-  if answer ==  "back":
-    print('You  go back and just lost.')
-
- if answer == "across":
- answer = input ('You crossed the bridge and meet a stranger .Do you talk to them (Yes /No)')
- 
- if answer == 'Yes':
-    print( 'You talk to the stranger and they give you gold. You WIN !!')
-  
- elif answer == 'No':
-    print('You ignore the stranger they are offended and you lose.')
- else:
-print('Not a valid option you lose')
-
-
-print('Thank you for trying ', name)
-    
+print("Thank you for trying", name + "!")
