9ad3708504778e9fd35d7dd1005df095019f42db
Popeaura <popeahura@gmail.com>
2024-12-07 13:05:10 +0300

    I added my modified projec's folder

--- modules/math.py
+++ modules/math.py
@@ -2,3 +2,19 @@ import math
 
 x = math.sqrt(36)
 print(x)
+
+#import  module by renaming it
+import math as m
+print(m.pi)
+
+#import only pi from math module
+from math import pi
+print(pi)
+
+#import all names from the standard module math
+from math import *
+
+print("The value of pi is ", pi)
+
+
+
---
+++ modules/test_numpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+print(a)
+
+b = np.array([[4, 5, 6], [7, 8, 9]])  # Corrected list format
+print(b)
