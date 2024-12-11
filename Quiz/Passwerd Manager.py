2f3d96742c66ac6a4af708e1e397cd457c13c985
Popeaura <popeahura@gmail.com>
2024-12-11 22:51:29 +0300

    I added my modified projec's folder

--- Quiz/Passwerd Manager.py
+++ Quiz/Passwerd Manager.py
@@ -3,3 +3,5 @@ pwd = input ('What is the master Password ??')
 mode = input('Would you like to add a new password or view an existing one (view/ add)')
 if mode == 'view':
     pass
+elif mode == 'add':
+    pass
