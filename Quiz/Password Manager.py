a83af0c0af4abebe86e065d0d3b7021a39d785ad
Popeaura <popeahura@gmail.com>
2024-12-12 23:28:03 +0300

    I added my modified projec's folder

--- Quiz/Password Manager.py
+++ Quiz/Password Manager.py
@@ -3,7 +3,8 @@ master_pwd = input('What is the master Password ??')
 def view():
       with open('passwords.txt', 'r') as f:
         for line in f.readlines():
-            print(line)
+            print(line.rstrip())
+
 def add():
     name = input('Account Name:')
     pwd = input('Password:')
