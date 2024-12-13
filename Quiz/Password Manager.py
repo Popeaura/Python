ef8e45397d715a461f4e0966ea6f5a40cb7f7c95
Popeaura <popeahura@gmail.com>
2024-12-13 23:50:03 +0300

    I added my modified projec's folder

--- Quiz/Password Manager.py
+++ Quiz/Password Manager.py
@@ -4,7 +4,8 @@ master_pwd = input('What is the master password? ')
 
 def write_key():
     key = Fernet.generate_key()
-    with open("key .key", "wb") as key 
+    with open("key .key", "wb") as key_file:
+        key_file.write(key)
 
 def view():
     try:
