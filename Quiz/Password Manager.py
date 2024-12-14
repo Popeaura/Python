48a5860441abc63c42b976600b7c487d8ba507ff
Popeaura <popeahura@gmail.com>
2024-12-14 23:36:42 +0300

    I added my modified projec's folder

--- Quiz/Password Manager.py
+++ Quiz/Password Manager.py
@@ -9,7 +9,7 @@ def write_key():
         key_file.write(key)'''
 
 def load_key():
-    return open("key.key")
+    return open("key.key" "rb")
 def view():
     try:
         with open('passwords.txt', 'r') as f:
