0b5b18e51817982cbe44de8303b5eb64a58819ea
Popeaura <popeahura@gmail.com>
2024-12-20 23:00:54 +0300

    I added my modified projec's folder

--- Quiz/Password Manager.py
+++ Quiz/Password Manager.py
@@ -1,75 +1,53 @@
 from cryptography.fernet import Fernet
 
-# Function to generate the encryption key
+
+'''
 def write_key():
     key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)
+    with open("key .key", "wb") as key_file:
+        key_file.write(key)'''
 
-# Function to load the encryption key
 def load_key():
-    try:
-        with open("key.key", "rb") as key_file:
-            key = key_file.read()
-        return key
-    except FileNotFoundError:
-        print("Key file not found! Please run the 'write_key()' function to generate the key.")
-        exit()
+    file =  open("key.key" "rb")
+    key = file.read()
+    file.close()
+    return key 
 
-# Generate the encryption object using the key and master password
-def get_fernet(master_pwd):
-    key = load_key()
-    full_key = key + master_pwd.encode()
-    try:
-        return Fernet(full_key[:32])  # Fernet key must be exactly 32 bytes
-    except ValueError:
-        print("Invalid master password or key.")
-        exit()
+master_pwd = input('What is the master password? ')
 
-# Functions for viewing and adding passwords
-def view(fer):
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+
+def view():
     try:
         with open('passwords.txt', 'r') as f:
             for line in f.readlines():
                 data = line.rstrip()
                 user, passw = data.split("|")
-                decrypted_pwd = fer.decrypt(passw.encode()).decode()
-                print(f"User: {user}, Password: {decrypted_pwd}")
+                print("User:", user, ", Password:",str(fer.decrypt(pwd.encode())) + "\n")
     except FileNotFoundError:
         print("No passwords stored yet!")
     except ValueError:
         print("The file format is incorrect. Please check the file contents.")
 
-def add(fer):
-    name = input("Account Name: ")
-    pwd = input("Password: ")
-    encrypted_pwd = fer.encrypt(pwd.encode()).decode()
+def add():
+    name = input('Account Name: ')
+    pwd = input('Password: ')
 
     with open('passwords.txt', 'a') as f:
-        f.write(f"{name}|{encrypted_pwd}\n")
-    print("Password saved successfully.")
+        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
 
-# Main logic for interacting with the program
-def main():
-    print("Welcome to the Password Manager!")
+
+
+while True:
+    mode = input('Would you like to add a new password or view existing ones? (view/add), press q to quit: ').lower()
+    if mode == 'q':
+        break
     
-    master_pwd = input("What is the master password? ")
-    fer = get_fernet(master_pwd)
-
-    while True:
-        mode = input("Would you like to add a new password or view existing ones? (view/add), press q to quit: ").lower()
-        if mode == 'q':
-            print("Goodbye!")
-            break
-        elif mode == 'view':
-            view(fer)
-        elif mode == 'add':
-            add(fer)
-        else:
-            print("Invalid mode. Please choose 'view', 'add', or 'q'.")
-
-# Uncomment the next line to generate the key file (only do this once!)
-# write_key()
-
-if __name__ == "__main__":
-    main()
+    if mode == 'view':
+        view()
+    elif mode == 'add':
+        add()
+    else:
+        print('Invalid mode')
